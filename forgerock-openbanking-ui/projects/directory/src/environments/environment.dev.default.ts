d09edec2f2bbe54d3f56f40c4e656c152f73b68c
Julien Renaux <julien.renaux@forgerock.com>
2020-03-12 17:13:21 +0100

    fix typo

--- forgerock-openbanking-ui/projects/directory/src/environments/environment.dev.default.ts
+++ forgerock-openbanking-ui/projects/directory/src/environments/environment.dev.default.ts
@@ -8,7 +8,7 @@ import { environment as defaultEnv } from './environment.default';
 
 export const environment = {
   ...defaultEnv,
-  production: false
+  production: false,
   devModules: [
     StoreDevtoolsModule.instrument({
       maxAge: 25,
